9e0e07aa0a147be930dd8cc68d7ea229df6982d5
christiansuar <148103464+christiansuar@users.noreply.github.com>
2023-11-02 10:49:17 -0500

    added question blocks
    added question blocks that give the player a beer

--- questionblock.gd
+++ questionblock.gd
@@ -11,4 +11,21 @@ func _on_body_entered(body):
 	if body.is_in_group("Player") and state == State.UNBUMPED:
 		bump_block()
 		
+# logic for bumping the block
 
+func bump_block():
+	state = State.BUMPED
+	$Sprite2D.frame = 1 # switch to the second frame to indicate "used" state
+	Global.spawn_beer_bottle(self.global_position + Vector2(0, -20))
+	bump_upwards()
+	var timer = get_tree().create_timer(0.2)
+	await timer.timeout
+	return_to_original_position()
+	
+	# moves block upwards
+func bump_upwards():
+		position.y -=10
+
+#returns block to its original positoin
+func return_to_original_position():
+	position = original_position
